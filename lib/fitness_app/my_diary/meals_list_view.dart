5c0b582438d71c635432957ca22215dbdb6da113
Ahmad Ma'sum <ahmadmasum0723@gmail.com>
2025-09-15 12:42:38 +0700

    first commit

---
+++ lib/fitness_app/my_diary/meals_list_view.dart
@@ -0,0 +1,279 @@
+import 'package:project4/fitness_app/fitness_app_theme.dart';
+import 'package:project4/fitness_app/models/student_data.dart';
+import 'package:project4/fitness_app/my_diary/student_detail_page.dart';
+import 'package:project4/main.dart';
+import 'package:flutter/material.dart';
+
+class MealsListView extends StatefulWidget {
+  const MealsListView({
+    Key? key,
+    this.mainScreenAnimationController,
+    this.mainScreenAnimation,
+  }) : super(key: key);
+
+  final AnimationController? mainScreenAnimationController;
+  final Animation<double>? mainScreenAnimation;
+
+  @override
+  _MealsListViewState createState() => _MealsListViewState();
+}
+
+class _MealsListViewState extends State<MealsListView>
+    with TickerProviderStateMixin {
+  AnimationController? animationController;
+  List<StudentData> studentsListData = StudentData.studentsList;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      duration: const Duration(milliseconds: 2000),
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  Future<bool> getData() async {
+    await Future<dynamic>.delayed(const Duration(milliseconds: 50));
+    return true;
+  }
+
+  @override
+  void dispose() {
+    animationController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: widget.mainScreenAnimationController!,
+      builder: (BuildContext context, Widget? child) {
+        return FadeTransition(
+          opacity: widget.mainScreenAnimation!,
+          child: Transform(
+            transform: Matrix4.translationValues(
+              0.0,
+              30 * (1.0 - widget.mainScreenAnimation!.value),
+              0.0,
+            ),
+            child: Container(
+              height: 216,
+              width: double.infinity,
+              child: ListView.builder(
+                padding: const EdgeInsets.only(
+                  top: 0,
+                  bottom: 0,
+                  right: 16,
+                  left: 16,
+                ),
+                itemCount: studentsListData.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  final int count = studentsListData.length > 10
+                      ? 10
+                      : studentsListData.length;
+                  final Animation<double> animation =
+                      Tween<double>(begin: 0.0, end: 1.0).animate(
+                        CurvedAnimation(
+                          parent: animationController!,
+                          curve: Interval(
+                            (1 / count) * index,
+                            1.0,
+                            curve: Curves.fastOutSlowIn,
+                          ),
+                        ),
+                      );
+                  animationController?.forward();
+
+                  return StudentView(
+                    studentData: studentsListData[index],
+                    animation: animation,
+                    animationController: animationController!,
+                  );
+                },
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class StudentView extends StatelessWidget {
+  const StudentView({
+    Key? key,
+    this.studentData,
+    this.animationController,
+    this.animation,
+  }) : super(key: key);
+
+  final StudentData? studentData;
+  final AnimationController? animationController;
+  final Animation<double>? animation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animationController!,
+      builder: (BuildContext context, Widget? child) {
+        return FadeTransition(
+          opacity: animation!,
+          child: Transform(
+            transform: Matrix4.translationValues(
+              100 * (1.0 - animation!.value),
+              0.0,
+              0.0,
+            ),
+            child: SizedBox(
+              width: 130,
+              child: Stack(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 32,
+                      left: 8,
+                      right: 8,
+                      bottom: 16,
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(
+                            color: HexColor(
+                              studentData!.endColor,
+                            ).withOpacity(0.6),
+                            offset: const Offset(1.1, 4.0),
+                            blurRadius: 8.0,
+                          ),
+                        ],
+                        gradient: LinearGradient(
+                          colors: <HexColor>[
+                            HexColor(studentData!.startColor),
+                            HexColor(studentData!.endColor),
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                        borderRadius: const BorderRadius.only(
+                          bottomRight: Radius.circular(8.0),
+                          bottomLeft: Radius.circular(8.0),
+                          topLeft: Radius.circular(8.0),
+                          topRight: Radius.circular(54.0),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          top: 54,
+                          left: 16,
+                          right: 16,
+                          bottom: 8,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              studentData!.namaLengkap,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: FitnessAppTheme.fontName,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                letterSpacing: 0.2,
+                                color: FitnessAppTheme.white,
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                  top: 8,
+                                  bottom: 8,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      'NISN: ${studentData!.nisn}\nKelas: XII IPA 1',
+                                      style: TextStyle(
+                                        fontFamily: FitnessAppTheme.fontName,
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 10,
+                                        letterSpacing: 0.2,
+                                        color: FitnessAppTheme.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => StudentDetailPage(
+                                      studentData: studentData!,
+                                    ),
+                                  ),
+                                );
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: FitnessAppTheme.nearlyWhite,
+                                  shape: BoxShape.circle,
+                                  boxShadow: <BoxShadow>[
+                                    BoxShadow(
+                                      color: FitnessAppTheme.nearlyBlack
+                                          .withOpacity(0.4),
+                                      offset: Offset(8.0, 8.0),
+                                      blurRadius: 8.0,
+                                    ),
+                                  ],
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(6.0),
+                                  child: Icon(
+                                    Icons.visibility,
+                                    color: HexColor(studentData!.endColor),
+                                    size: 24,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    child: Container(
+                      width: 84,
+                      height: 84,
+                      decoration: BoxDecoration(
+                        color: FitnessAppTheme.nearlyWhite.withOpacity(0.2),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    left: 8,
+                    child: SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: Image.asset(studentData!.imagePath),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
